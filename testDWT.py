8825497dad3e75ab20c22b59847ec403ddcc7c17
NBVA2002 <117577336+NBVA2002@users.noreply.github.com>
2023-03-17 19:41:25 +0700

    Update testDWT.py

--- testDWT.py
+++ testDWT.py
@@ -4,16 +4,8 @@ import pywt
 import matplotlib.pyplot as plt
 
 def embed_image(img, watermark, alpha):
-    # white_img = np.zeros_like(img)
-
-    # # Vị trí nhúng
-    # x_offset = 450
-    # y_offset = 650
-    # watermark = cv2.resize(watermark, (100, 100))
-    # white_img[y_offset : y_offset + watermark.shape[0], x_offset : x_offset + watermark.shape[1]] = watermark
-    
-    # # Hiển thị vị trí được nhúng
-    # cv2.imshow('Vi tri nhung', white_img)
+    cv2.imshow('Anh goc',img)
+    cv2.imshow('QR goc goc',cv2.resize(watermark, (300, 300)))
     
     # Resize ảnh được nhúng bằng kích thước ảnh gốc
     watermark = cv2.resize(watermark, (img.shape[1], img.shape[0]))
@@ -33,7 +25,7 @@ def embed_image(img, watermark, alpha):
     
     return embed_img
 
-def extract_image(embed_img, original_img):
+def extract_image(embed_img, original_img, alpha):
     # Biến đổi DWT vói ảnh đã được nhúng và ảnh gốc
     coeffs_embed = pywt.dwt2(embed_img, 'haar')
     coeffs_orig = pywt.dwt2(original_img, 'haar')
@@ -42,15 +34,12 @@ def extract_image(embed_img, original_img):
     cA_orig, (cH_orig, cV_orig, cD_orig) = coeffs_orig
 
     # trích xuất ảnh
-    alpha = 0.001
     cA_extract = (cA_embed - cA_orig) / alpha
 
     # Biên đổi ngược DWT để nhận được ảnh trích xuất
     extracted_img = pywt.idwt2((cA_extract, (cH_orig, cV_orig, cD_orig)), 'haar')
     
     # Resize ảnh sau khi lấy được kết quả
-    
-    # extracted_img = extracted_img[650:750, 450:550]
     extracted_img = cv2.resize(extracted_img, (300, 300))
     
     return extracted_img
@@ -60,16 +49,20 @@ if __name__ == "__main__":
     img = cv2.imread('13.jpg')
     watermark = cv2.imread('QRcode.png')
     
-    # Độ mờ 
+    # Khóa alpha 
     alpha = 0.02
 
     # Nhúng ảnh 
     embedded_img = embed_image(img, watermark, alpha)
+    
+    # lưu ảnh nhúng và hiển thị ở dạng uint8
     cv2.imwrite('DWTembedded_img.png', embedded_img)
     cv2.imshow('Embedded Image', cv2.imread('DWTembedded_img.png'))
     
     # Trích xuất QR code 
-    extracted_img = extract_image(embedded_img, img)
+    extracted_img = extract_image(embedded_img, img, alpha)
+    
+    # lưu ảnh trích xuất và hiển thị ở dạng uint8
     cv2.imwrite('DWTextracted_watermark.png', extracted_img)
     cv2.imshow('Extracted Image', cv2.imread('DWTextracted_watermark.png'))
     
